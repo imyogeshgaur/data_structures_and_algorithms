60ae4bb382b5ff290eb12246ae430d2186511709
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-05 15:32:18 +0530

    learn how to delete data in a BST

---
+++ implelement_in_cpp/Trees/Binary_Search_Tree/deletion_in_BST.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+struct tree
+{
+    int value;
+    tree *left, *right;
+    tree(int x)
+    {
+        value = x;
+        left = right = NULL;
+    }
+};
+
+tree *insertInBST(tree *head, int data)
+{
+    if (head == NULL)
+    {
+        return new tree(data);
+    }
+    else if (head->value > data)
+    {
+        head->left = insertInBST(head->left, data);
+    }
+    else
+    {
+        head->right = insertInBST(head->right, data);
+    }
+    return head;
+}
+tree *deleteInBST(tree *head, int data)
+{
+    if (head == NULL)
+    {
+        return head;
+    }
+    if (head->value > data)
+    {
+        head->left = deleteInBST(head->left, data);
+    }
+    else if (head->value < data)
+    {
+        head->right = deleteInBST(head->right, data);
+    }
+    else
+    {
+        if (head->left == NULL)
+        {
+            tree *temp = head->right;
+            delete head;
+            return temp;
+        }
+        else if (head->right == NULL)
+        {
+            tree *temp = head->left;
+            delete head;
+            return temp;
+        }
+    }
+}
+void traverse_tree(tree *head)
+{
+    if (head != NULL)
+    {
+        traverse_tree(head->left);
+        cout << head->value << " ";
+        traverse_tree(head->right);
+    }
+}
+
+int main()
+{
+    tree *root = NULL;
+    root = insertInBST(root, 30);
+    insertInBST(root, 20);
+    insertInBST(root, 40);
+    insertInBST(root, 10);
+    insertInBST(root, 25);
+    insertInBST(root, 35);
+    insertInBST(root, 50);
+
+    cout << "Tree Before Deletion (InOrder Traversal) : \n";
+    traverse_tree(root);
+    deleteInBST(root, 10);
+    cout << "\nTree After Deletion (InOrder Traversal) : \n";
+    traverse_tree(root);
+}
