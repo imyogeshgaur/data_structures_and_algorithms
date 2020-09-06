8395b975b0c55a99124f211fd0a059297bc03d76
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-06 08:46:44 +0530

    learn how to insert data into queue using array

--- implelement_in_cpp/Queue/enqueue_using_array.cpp
+++ implelement_in_cpp/Queue/enqueue_using_array.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 #define size 4
 
 int arr[size];
-int rear = - 1;
-int front = - 1;
+int rear = -1;
+int front = -1;
 
 void enqueue()
 {
-  int add_item;
-  if (rear == size-1)
-  {
-    cout<<"Queue Overflow \n";
-  }
+    int add_item;
+    if (rear == size - 1)
+    {
+        cout << "Queue Overflow \n";
+    }
     else
     {
-        if (front == - 1)
-        front = 0;
-        cout<<"Inset the element in queue : ";
+        if (front == -1)
+            front = 0;
+        cout << "Inset the element in queue : ";
         scanf("%d", &add_item);
         rear = rear + 1;
         arr[rear] = add_item;
@@ -28,25 +28,25 @@ void enqueue()
 void traversal_queue()
 {
     int i;
-    if (front == - 1)
+    if (front == -1)
     {
-        cout<<"Queue is empty \n";
+        cout << "Queue is empty \n";
     }
     else
     {
-        cout<<"Queue is : \n";
+        cout << "Queue is : \n";
         for (i = front; i <= rear; i++)
         {
-            cout<< arr[i]<<" ";
+            cout << arr[i] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
 int main()
 {
-   for(int i=0;i<size;i++)
-   {
-       enqueue();
-   }
-   traversal_queue();
+    for (int i = 0; i < size; i++)
+    {
+        enqueue();
+    }
+    traversal_queue();
 }
