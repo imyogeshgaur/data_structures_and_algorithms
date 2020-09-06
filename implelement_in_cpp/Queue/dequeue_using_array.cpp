8c370e5b18097b1ebd6e84e7163cbdbf88d53463
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-06 08:44:17 +0530

    learn how to delete data from queue using array

--- implelement_in_cpp/Queue/dequeue_using_array.cpp
+++ implelement_in_cpp/Queue/dequeue_using_array.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 #define size 4
 
 int arr[size];
-int rear = - 1;
-int front = - 1;
+int rear = -1;
+int front = -1;
 
 void enqueue()
 {
-  int add_item;
-  if (rear == size-1)
-  {
-    cout<<"Queue Overflow \n";
-  }
+    int add_item;
+    if (rear == size - 1)
+    {
+        cout << "Queue Overflow \n";
+    }
     else
     {
-        if (front == - 1)
-        front = 0;
-        cout<<"Inset the element in queue : ";
+        if (front == -1)
+            front = 0;
+        cout << "Inset the element in queue : ";
         scanf("%d", &add_item);
         rear = rear + 1;
         arr[rear] = add_item;
@@ -26,41 +26,44 @@ void enqueue()
 }
 void dequeue()
 {
-    if (front == - 1 || front > rear)
+    if (front == -1 || front > rear)
     {
-        cout<<"Queue Underflow"<<"\n";
-        return ;
+        cout << "Queue Underflow"
+             << "\n";
+        return;
     }
     else
     {
         front = front + 1;
     }
-} 
+}
 void traversal_queue()
 {
     int i;
-    if (front == - 1)
+    if (front == -1)
     {
-        cout<<"Queue is empty \n";
+        cout << "Queue is empty \n";
     }
     else
     {
         for (i = front; i <= rear; i++)
         {
-            cout<< arr[i]<<" ";
+            cout << arr[i] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
 int main()
 {
-   for(int i=0;i<size;i++)
-   {
-       enqueue();
-   }
-   cout<<"Queue before deletion :"<<"\n";
-   traversal_queue();
-   dequeue();
-   cout<<"Queue after deletion : "<<"\n";
-   traversal_queue();
+    for (int i = 0; i < size; i++)
+    {
+        enqueue();
+    }
+    cout << "Queue before deletion :"
+         << "\n";
+    traversal_queue();
+    dequeue();
+    cout << "Queue after deletion : "
+         << "\n";
+    traversal_queue();
 }
