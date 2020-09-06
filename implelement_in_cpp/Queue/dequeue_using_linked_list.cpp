ad9eab1cac1a76dd11a7e0217fc2b2ad6d0762f8
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-06 08:44:57 +0530

    learn how to delete data from queue using linked list

--- implelement_in_cpp/Queue/dequeue_using_linked_list.cpp
+++ implelement_in_cpp/Queue/dequeue_using_linked_list.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct queue
 {
-   int value;
-   struct queue *next;
-}*front,*rear,*temp,*fr;
+    int value;
+    struct queue *next;
+} * front, *rear, *temp, *fr;
 
 void enqueue(int data)
 {
@@ -18,69 +18,72 @@ void enqueue(int data)
     }
     else
     {
-        temp=(struct queue *)malloc(sizeof(struct queue));
+        temp = (struct queue *)malloc(sizeof(struct queue));
         rear->next = temp;
         temp->value = data;
         temp->next = NULL;
- 
+
         rear = temp;
     }
 }
 void deqeue()
 {
-     fr = front;
- 
+    fr = front;
+
     if (fr == NULL)
     {
-        cout<<"\nError: Trying to display elements from empty queue";
+        cout << "\nError: Trying to display elements from empty queue";
         return;
     }
+    else if (fr->next != NULL)
+    {
+        fr = fr->next;
+        free(front);
+        front = fr;
+    }
     else
-        if (fr->next != NULL)
-        {
-            fr = fr->next;
-            free(front);
-            front = fr;
-        }
-        else
-        {
-            free(front);
-            front = NULL;
-            rear = NULL;
-        }
+    {
+        free(front);
+        front = NULL;
+        rear = NULL;
+    }
 }
 void traverse_queue()
 {
     fr = front;
- 
+
     if ((fr == NULL) && (rear == NULL))
     {
-        cout<<"Queue is empty";
+        cout << "Queue is empty";
         return;
     }
     while (fr != rear)
     {
-        cout<<fr->value<<" ";
+        cout << fr->value << " ";
         fr = fr->next;
     }
     if (fr == rear)
-        cout<<fr->value<<" ";
+        cout << fr->value << " ";
 }
 
 int main()
 {
-  int size,elem;
-  cout<<"Enter the size of the Queue "<<" \n";
-  scanf("%d",&size);
-  for(int i=0;i<size;i++)
-  {
-      cout<<"Enter the Element "<<"\n";
-      scanf("%d",&elem);
-      enqueue(elem);
-  }
-  cout<<"Queue before deletion is "<<"\n";
-  traverse_queue();
-  deqeue();
-  cout<<"\nQueue After deletion is "<<"\n";
-  traverse_queue();
+    int size, elem;
+    cout << "Enter the size of the Queue "
+         << " \n";
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter the Element "
+             << "\n";
+        scanf("%d", &elem);
+        enqueue(elem);
+    }
+    cout << "Queue before deletion is "
+         << "\n";
+    traverse_queue();
+    deqeue();
+    cout << "\nQueue After deletion is "
+         << "\n";
+    traverse_queue();
 }
